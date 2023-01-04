2b25332f5cd3ea37a74108e5e864ae6c752451ad
Karen Douglas <kdindependence@gmail.com>
2023-01-04 10:37:47 -0600

    styled FAQ component for medium-small screen sizes

--- app/routes/about/index.tsx
+++ app/routes/about/index.tsx
@@ -1,5 +1,5 @@
 // helpers
-import { json } from "@remix-run/node"; // or cloudflare/deno
+import { json } from "@remix-run/node"; 
 import { useLoaderData } from "@remix-run/react";
 import type {LinksFunction } from "@remix-run/node";
 // styles
@@ -8,7 +8,8 @@ import aboutSharedStyles from  "css/pages/about/aboutShared.css";
 import aboutMobileStyles from  "css/pages/about/aboutMobile.css";
 import aboutDesktopStyles from  "css/pages/about/aboutDesktop.css";
 import PageTitle from "~/components/PageTitle";
-import faqSharedStyles from "css/components/faqShared.css"
+import faqSharedStyles from "css/components/faqShared.css";
+import faqTabletStyles from "css/components/faqTablet.css";
 // images and svgs
 import aboutHeroDesktop from "~/assets/images/about-hero-desktop.jpg";
 import aboutHeroTablet from "~/assets/images/about-hero-tablet.jpg";
@@ -22,7 +23,7 @@ import PageLayoutStyle1 from "~/components/PageLayoutStyle1";
 import PageLayoutStyle2 from "~/components/PageLayoutStyle2";
 import Faqs from "~/components/Faqs";
 //data
-import {faqs} from "data/db.json"
+import {faqs} from "data/db.json";
 
 
 
@@ -31,6 +32,11 @@ export const links: LinksFunction = () => {
     {
       rel: "stylesheet",
       href: faqSharedStyles,
+    },
+    {
+      rel: "stylesheet",
+      href: faqTabletStyles,
+      media: "(max-width: 1031px)",
     },
     {
       rel: "stylesheet",
@@ -67,34 +73,31 @@ export default function Index():JSX.Element {
         title="About"
         />
         <section className="ps1_wrapper">
-        <PageLayoutStyle1
-        img={digitalEra}
-        arrowImg={<SvgLeftUpwardArrow/>}
-        arrowClass="page_left_up_arrow"
-        title=" Mobility for the digital era"
-        text="Getting around should be simple (and even fun!) for everyone. We embrace technology to 
-        provide low cost, smart access to scooters at your fingertips."
-        button={false}
+          <PageLayoutStyle1
+          img={digitalEra}
+          arrowImg={<SvgLeftUpwardArrow/>}
+          arrowClass="page_left_up_arrow"
+          title=" Mobility for the digital era"
+          text="Getting around should be simple (and even fun!) for everyone. We embrace technology to 
+          provide low cost, smart access to scooters at your fingertips."
+          button={false}
 
-        />
-         <PageLayoutStyle1
-      img={betterLiving}
-      arrowImg={<SvgRightArrow/>}
-      arrowClass="page_right_arrow"
-      title="Better urban living"
-      text="We’re helping connect cities and bring people closer together. Our scooters are also 
-      fully-electric and we offset the minimal carbon footprint for each ride."
-      button={false}
-      />
-        </section>
-        <section>
-          <PageLayoutStyle2
-          title="Our Values"
+          />
+          <PageLayoutStyle1
+          img={betterLiving}
+          arrowImg={<SvgRightArrow/>}
+          arrowClass="page_right_arrow"
+          title="Better urban living"
+          text="We’re helping connect cities and bring people closer together. Our scooters are also 
+          fully-electric and we offset the minimal carbon footprint for each ride."
+          button={false}
           />
         </section>
         <section>
-        <Faqs
-        data={faqs}/>
+          <PageLayoutStyle2 title="Our Values"/>
+        </section>
+        <section>
+          <Faqs data={faqs}/>
         </section>
       </div>
     );
